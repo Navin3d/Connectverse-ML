9451a768364552c0292461bfcc8fbbfed683038c
Navin3d <smnavin65@mail.com>
2022-08-25 09:09:40 +0530

    ml model

--- wage_calculation/urls.py
+++ wage_calculation/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
          views.get_wage_amount),
     path("working_hours/<str:age_input>/<str:gender_input>/<str:wage_input>/<str:workType_input>/<str:physicalHealthPoints>",
          views.get_work_hours),
+    path("can-apply/<str:payPerHour_input>/<str:workHoursPerWeekGreatethan21Hours_input>/<str:hasDrivingLicense_input>/<str:hasvehicle_input>/<str:waitingTime_input>/<str:blokSameAsJob_input>/<str:districtSameAsJob_input>/<str:stateSameAsJob_input>/<str:readyToRelocate_input>/",
+         views.can_apply_for_job),
 ]
--- wage_calculation/views.py
+++ wage_calculation/views.py
@@ -5,27 +5,31 @@ from rest_framework.decorators import api_view
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
-
 # Create your views here.
 # https://navin3d.github.io/Connectverse-Staticfiles/staticfiles/Money_Calculation.csv
 # https://navin3d.github.io/Connectverse-Staticfiles/staticfiles/Work_Time_Calculation.csv
+# https://navin3d.github.io/Connectverse-Staticfiles/staticfiles/Job_Assign.csv
 
 wage_data = pd.read_csv('https://navin3d.github.io/Connectverse-Staticfiles/staticfiles/Money_Calculation.csv')
-
 wage_X = wage_data.drop(columns=['wagePerHr'])
 wage_Y = wage_data['wagePerHr']
-
 wage_model = DecisionTreeClassifier()
 wage_model.fit(wage_X, wage_Y)
 
 
 working_hrs_data = pd.read_csv('https://navin3d.github.io/Connectverse-Staticfiles/staticfiles/Work_Time_Calculation.csv')
-
 working_hrs_X = working_hrs_data.drop(columns=['WorkingHrsMoreThan40PerWeek'])
 working_hrs_Y = working_hrs_data['WorkingHrsMoreThan40PerWeek']
-
 working_hrs_model = DecisionTreeClassifier()
 working_hrs_model.fit(working_hrs_X, working_hrs_Y)
+
+
+job_assign_data = pd.read_csv('https://navin3d.github.io/Connectverse-Staticfiles/staticfiles/Job_Assign.csv')
+job_assign_X = job_assign_data.drop(columns=['canApplyToJob'])
+job_assign_Y = job_assign_data['canApplyToJob']
+print(job_assign_X, job_assign_Y)
+job_assign_model = DecisionTreeClassifier()
+job_assign_model.fit(job_assign_X, job_assign_Y)
 
 
 def decode_gender(gender):
@@ -72,6 +76,11 @@ def predict_work_hrs(age, gender_encoded, wage, workType_encoded, physicalHealth
     return working_hrs[0]
 
 
+def predict_can_apply(payPerHour, workHoursPerWeekGreatethan21Hours, hasDrivingLicense, hasvehicle, waitingTime, blokSameAsJob, districtSameAsJob, stateSameAsJob, readyToRelocate):
+    can_apply_job = job_assign_model.predict([[payPerHour, workHoursPerWeekGreatethan21Hours, hasDrivingLicense, hasvehicle, waitingTime, blokSameAsJob, districtSameAsJob, stateSameAsJob, readyToRelocate], [20, 1, 1, 0, 7, 0, 0, 0, 0]])
+    return can_apply_job[0]
+
+
 @api_view(["GET"])
 def get_wage_amount(request, age_input, gender_input, WorkingHrsMoreThan40PerWeek_input, workType_input):
     if request.method == "GET":
@@ -84,3 +93,10 @@ def get_work_hours(request, age_input, gender_input, wage_input, workType_input,
     if request.method == "GET":
         working_hrs = predict_work_hrs(age_input, gender_input, wage_input, workType_input, physicalHealthPoints)
         return Response(working_hrs, status=status.HTTP_200_OK)
+
+
+@api_view(["GET"])
+def can_apply_for_job(request, payPerHour_input, workHoursPerWeekGreatethan21Hours_input, hasDrivingLicense_input, hasvehicle_input, waitingTime_input, blokSameAsJob_input, districtSameAsJob_input, stateSameAsJob_input, readyToRelocate_input):
+    if request.method == "GET":
+        can_apply = predict_can_apply(payPerHour_input, workHoursPerWeekGreatethan21Hours_input, hasDrivingLicense_input, hasvehicle_input, waitingTime_input, blokSameAsJob_input, districtSameAsJob_input, stateSameAsJob_input, readyToRelocate_input)
+        return Response(can_apply, status=status.HTTP_200_OK)
