4070612d2c2ff2f5c07baa3e9fd6f6daecd321fc
Navin3d <smnavin65@mail.com>
2022-08-26 18:42:32 +0530

    final

--- wage_calculation/urls.py
+++ wage_calculation/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("wage/<str:age_input>/<str:gender_input>/<str:WorkingHrsMoreThan40PerWeek_input>/<str:workType_input>",
+        path("wage/<str:age_input>/<str:gender_input>/<str:WorkingHrsMoreThan40PerWeek_input>/<str:workType_input>",
          views.get_wage_amount),
     path("working_hours/<str:age_input>/<str:gender_input>/<str:wage_input>/<str:workType_input>/<str:physicalHealthPoints>",
          views.get_work_hours),
